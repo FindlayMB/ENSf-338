acae3126fc9b552dbf61712ac1aee3292e33b126
FindlayMB <findlay.brown@ucalgary.ca>
2023-01-18 12:05:59 -0700

    Finished Question 3 Edited Question 8

--- Assignment1/Q3/Question3.py
+++ Assignment1/Q3/Question3.py
@@ -1,29 +1,24 @@
 import matplotlib.pyplot as plt
-
 def main():
     alphabet =  [0 for i in range(26)] # index 0 = A, index 25 = Z
-    
     with open('data.txt','r', encoding="utf8") as f:
         while True:
-            char = f.read(1)
-            if char.isalpha(): char = char.upper() 
+            char = f.read(1).upper()
             if(not char):
                 break
             alphabetIndex = ord(char) - 65
             if alphabetIndex in range(26):
                 alphabet[alphabetIndex] += 1
-    
     totalLetters = sum(alphabet)
     frequency = [x/totalLetters for x in alphabet]
     for i,x in enumerate(alphabet):
-        print(f'Letter: {chr(i+65)}\tTotal: {x}\tFrequency: {frequency[i]}')
+        print(f'Letter: {chr(i+65)}\tFrequency: {frequency[i]}')
     print(f'All Letters\tTotal: {totalLetters}')
-    #plt.hist([[chr(i+65),x] for i,x in enumerate(alphabet)])
-    #plt.hist(frequency,align='left')
-    plt.bar(x=range(26),height=alphabet,tick_label=[chr(i) for i in range(65,91)])
+    plt.bar(x=range(26),height=frequency,tick_label=[chr(i) for i in range(65,91)])
+    plt.xlabel('Letters')
+    plt.ylabel('Frequency')
+    plt.title('Letter frequency of The Brothers Karamazov by Fyodor Dostoyevsky')
     plt.show()
-
 
 if __name__ == '__main__':
     main()
-
--- Assignment1/Q8/Question8.py
+++ Assignment1/Q8/Question8.py
@@ -1,11 +1,10 @@
 from time import perf_counter
 
 timeStart = perf_counter()
+
 writeFile = open('write.txt','a',encoding='utf-8')
 readFile = open('read.txt','r',encoding='utf-8')
-
-for i in readFile.read().upper():
-    writeFile.write(i)
+writeFile.write(readFile.read().upper())
 
 timeStop = perf_counter()
 
