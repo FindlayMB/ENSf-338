eb530547ddfa88a2aa76d8eade328ca9e5ef22b8
[gracejang] <grace.jang1@ucalgary.ca>
2023-03-14 17:55:07 -0600

    excercise 3 tweaked

--- Assignments/Assignment3/exercise3/ex3.3.py
+++ Assignments/Assignment3/exercise3/ex3.3.py
@@ -1,11 +1,12 @@
 import sys
 
-old_capacity = 0
 lst = []
+capacity = sys.getsizeof(lst)
+print(f"List capacity: {capacity} bytes")
 
 for i in range(64):
     lst.append(i)
     new_capacity = sys.getsizeof(lst)
-    if new_capacity != old_capacity:
-        print(f"Capacity changed from {old_capacity} bytes to {new_capacity} bytes at {i} elements")
-        old_capacity = new_capacity
+    if new_capacity != capacity:
+        print(f"List capacity changed from {capacity} bytes to {new_capacity} bytes at size {i}")
+        capacity = new_capacity
